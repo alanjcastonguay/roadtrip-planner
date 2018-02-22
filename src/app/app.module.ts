4f5ca2a8ddce9ac4044012fb0d83f42714b15826
Alan J Castonguay <alanjcastonguay@gmail.com>
2018-02-22 17:31:56 -0500

    Dependency on https://angular-maps.com/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { MatInputModule } from '@angular/material/input';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { FormComponent } from './form/form.component';
+import { AgmCoreModule } from '@agm/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,13 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
-    MatInputModule
+    MatInputModule,
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_KEY'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
