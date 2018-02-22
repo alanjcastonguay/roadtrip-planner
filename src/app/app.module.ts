87eea12adbd9cdbcb9cd70cc2d12e2ad5f17e626
Daniel de Sao Jose <danieldsj@gmail.com>
2018-02-22 17:15:46 -0500

    Created form angularjs component based on examples.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
